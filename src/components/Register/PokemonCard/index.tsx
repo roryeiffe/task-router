775307e3f118a29ba746838de1e5caad0163bf77
roryeiffe <roryeiffe8@gmail.com>
2021-12-06 16:42:11 -0500

    Updated project.txt

--- src/components/Register/PokemonCard/index.tsx
+++ src/components/Register/PokemonCard/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Pokemon } from "pokenode-ts";
 import {useState, useEffect} from "react";
+import './style.css';
 
 const PokemonCard = (props:any) => {
   const changeStarterId = () => {
@@ -8,20 +9,23 @@ const PokemonCard = (props:any) => {
   }
 
   const [borderStyle, setBorderStyle] = useState({border: "1px solid #ccc"});
+  const [imgUrl, setImgUrl] = useState(props.pokemon.sprites.back_default);
   
   useEffect(() => {
     if(props.user.starterId === props.pokemon.id) {
       setBorderStyle({border: "4px solid #000",});
+      setImgUrl(props.pokemon.sprites.front_default);
     }
     else {
       setBorderStyle({border: "1px solid #ccc"});
+      setImgUrl(props.pokemon.sprites.back_default);
     }
   }, [props.pokemon.id, props.user.starterId]);
 
   return (
-    <div className = "col-lg-4">
+    <div className = "col-lg-4 poke-card">
       <div className="card" style = {borderStyle} onClick = {() => changeStarterId()}>
-        <img draggable = 'false' src={props.pokemon.sprites.front_default} className="card-img-top img" alt="..." />
+        <img draggable = 'false' src={imgUrl} className="card-img-top img" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{props.pokemon.name}</h5>
           <p className="card-text">
