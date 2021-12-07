2ea46ca45e7efcfc86de6fa8ae3090847268f4b3
asalvero <ajalvero1@gmail.com>
2021-12-06 22:29:43 -0500

    Made some changes to the css page and included it in both the Task page and component folders.

--- src/components/Tasks/TaskHeader/index.tsx
+++ src/components/Tasks/TaskHeader/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from '../style.module.css'
 import Button from '../TaskButton'
 
 const Header = ({onAdd, showAdd}: any) => {
     return (
-        <header className='header'>
+        <header className={styles.header}>
             <h1>Tasks</h1>
             <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
         </header>
