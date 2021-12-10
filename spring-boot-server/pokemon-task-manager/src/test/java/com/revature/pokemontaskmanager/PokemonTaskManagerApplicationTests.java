0d5899867e64f72ca0f3d1c4f721bd9dd9319031
roryeiffe <roryeiffe8@gmail.com>
2021-12-10 16:47:59 -0500

    Reorganized file structure to have front-end and back-end. Started a spring-boot app for the backend. Created different entities and a user controller.

---
+++ spring-boot-server/pokemon-task-manager/src/test/java/com/revature/pokemontaskmanager/PokemonTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.pokemontaskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
