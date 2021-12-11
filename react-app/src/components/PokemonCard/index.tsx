3895eb7ee2d470ecc1b8ac6c02b06e9c29e60e2e
roryeiffe <roryeiffe8@gmail.com>
2021-12-11 01:20:33 -0500

    Added functionality to change starter pokemon sprite when clicking on it

--- react-app/src/components/PokemonCard/index.tsx
+++ react-app/src/components/PokemonCard/index.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { useState } from 'react';
 import {IPokemon} from '../../interfaces/Pokemon';
 
 interface IProps {
@@ -7,13 +8,33 @@ interface IProps {
 
 const PokemonCard = (props: any) => {
 const user = useSelector((state:any) => state.user);
+
+  const [sprite, setSprite] = useState(props.pokemon.sprites.front_default);
+
+  const changeSprite = () => {
+    // get a random sprite until find one that isn't null:
+    let possible_sprites = ["back_default", "back_female", "back_shiny", "back_shiny_female", "front_default", "front_female", "front_shiny", "front_shiny_female"];
+    while(true) {
+      let index = Math.floor(Math.random() * possible_sprites.length)
+      let spriteType = possible_sprites[index];
+      var newSprite = props.pokemon.sprites[spriteType];
+      if(newSprite !== null) {
+        break;
+      }
+    }
+    
+      setSprite(newSprite);
+  }
+
+
   return (
     <div className="card">
       <img
-        src={props.pokemon.sprites.front_default}
+        src={sprite}
         className="card-img-top"
         alt="..."
         draggable="false"
+        onClick = {changeSprite}
       />
       <div className="card-body">
         <h4 className="card-title">{props.pokemon.name}</h4>
