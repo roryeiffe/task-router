de3d7733e7ecd0d832a7006f6537da47181a05f2
Cathy Nguyen <Smileycathy1128@gmail.com>
2021-12-11 17:17:18 -0500

    feature creep intensifies

--- react-app/src/PokeCorner/index.tsx
+++ react-app/src/PokeCorner/index.tsx
@@ -2,21 +2,9 @@
 import { PokemonClient } from 'pokenode-ts';
 import { useState } from 'react';
 
-const pokeApi = new PokemonClient();
+export const pokeApi = new PokemonClient();
 
-// function getPkmnNameByDexNoNoPromise(dexNo:number):String {
-//     getPkmnNameByDexNo(dexNo)
-//     .then(data => {
-//         return data as String
-//     })
-//     .catch(error => {
-//         // console.error(error);
-//         return error as String;
-//     });
-//     return "";
-// }
-
-async function getPkmnNameByDexNo(dexNo:number):Promise<string> {
+export async function getPkmnNameByDexNo(dexNo:number):Promise<string> {
     let name:String = 'missingNo.';
     
     await pokeApi
@@ -38,117 +26,118 @@ async function getPkmnNameByDexNo(dexNo:number):Promise<string> {
 // }
 
 // ======================================================================
-class getPokemonSprite {
-    static front(a:number):Promise<any>;
-    static front(a:string, ):Promise<any>;
-    static front(a:any):Promise<any> {
-        if(a as number) {
-            // getPkmnNameByDexNo(a).then(pkmnName => {
-            //     return <PokemonCard pokemon={pkmnName} />;
-            // });
-            return getPokemonFrontSpriteByDexNo(a);
-        }
-        else if(a as String) {
-            // return <PokemonCard pokemon={a} />
-            return getPokemonFrontSpriteByName(a);
-        }
-        else {
-            let tempStr = "type error"
-            console.error(tempStr);
-            return new Promise<String>(() => {return tempStr});
-        }
-    }
-    static back(a:number):Promise<any>;
-    static back(a:string, ):Promise<any>;
-    static back(a:any):Promise<any> {
-        if(a as number) {
-            return getPokemonBackSpriteByDexNo(a);
-        }
-        else if(a as String) {
-            return getPokemonBackSpriteByName(a);
-        }
-        else {
-            let tempStr = "type error"
-            console.error(tempStr);
-            return new Promise<String>(() => {return tempStr});
-        }
-    }
-}
-async function getPokemonFrontSpriteByDexNo(dexNo:number):Promise<any> {
-    await pokeApi
-    .getPokemonById(dexNo)
-    .then((data:any) => {
-        return data.sprites.front_default;
-    })
-    .catch(error => {
-        console.error(error);
-    });
-}
-async function getPokemonFrontSpriteByName(pkmnName:string):Promise<any> {
-    await pokeApi
-    .getPokemonByName(pkmnName)
-    .then((data:any) => {
-        return data.sprites.front_default;
-    })
-    .catch(error => {
-        console.error(error)
-    });
-}
-async function getPokemonBackSpriteByDexNo(pkmnName:string):Promise<any> {
-    await pokeApi
-    .getPokemonByName(pkmnName)
-    .then((data:any) => {
-        return data.sprites.back_default;
-    })
-    .catch(error => {
-        console.error(error);
-    });
-}
-async function getPokemonBackSpriteByName(pkmnName:string):Promise<any> {
-    await pokeApi
-    .getPokemonByName(pkmnName)
-    .then((data:any) => {
-        return data.sprites.back_default;
-    })
-    .catch(error => {
-        console.error(error);
-    });
-}
+// class getPokemonSprite {
+//     static front(a:number):Promise<any>;
+//     static front(a:string, ):Promise<any>;
+//     static front(a:any):Promise<any> {
+//         if(a as number) {
+//             // getPkmnNameByDexNo(a).then(pkmnName => {
+//             //     return <PokemonCard pokemon={pkmnName} />;
+//             // });
+//             return getPokemonFrontSpriteByDexNo(a);
+//         }
+//         else if(a as String) {
+//             // return <PokemonCard pokemon={a} />
+//             return getPokemonFrontSpriteByName(a);
+//         }
+//         else {
+//             let tempStr = "type error"
+//             console.error(tempStr);
+//             return new Promise<String>(() => {return tempStr});
+//         }
+//     }
+//     static back(a:number):Promise<any>;
+//     static back(a:string, ):Promise<any>;
+//     static back(a:any):Promise<any> {
+//         if(a as number) {
+//             return getPokemonBackSpriteByDexNo(a);
+//         }
+//         else if(a as String) {
+//             return getPokemonBackSpriteByName(a);
+//         }
+//         else {
+//             let tempStr = "type error"
+//             console.error(tempStr);
+//             return new Promise<String>(() => {return tempStr});
+//         }
+//     }
+// }
+// async function getPokemonFrontSpriteByDexNo(dexNo:number):Promise<any> {
+//     await pokeApi
+//     .getPokemonById(dexNo)
+//     .then((data:any) => {
+//         return data.sprites.front_default;
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+// }
+// async function getPokemonFrontSpriteByName(pkmnName:string):Promise<any> {
+//     await pokeApi
+//     .getPokemonByName(pkmnName)
+//     .then((data:any) => {
+//         return data.sprites.front_default;
+//     })
+//     .catch(error => {
+//         console.error(error)
+//     });
+// }
+// async function getPokemonBackSpriteByDexNo(pkmnName:string):Promise<any> {
+//     await pokeApi
+//     .getPokemonByName(pkmnName)
+//     .then((data:any) => {
+//         return data.sprites.back_default;
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+// }
+// async function getPokemonBackSpriteByName(pkmnName:string):Promise<any> {
+//     await pokeApi
+//     .getPokemonByName(pkmnName)
+//     .then((data:any) => {
+//         return data.sprites.back_default;
+//     })
+//     .catch(error => {
+//         console.error(error);
+//     });
+// }
 // ======================================================================
 
-// ======================================================================
-class getList {
-    // static allPokemon():string[] {}
-    static allGen1Pokemon():string[] {
-        while(true) { // TODO: '-'
-            getPokemonList(151).then(list => {
-                return list as string[];
-            });
-            console.error("allGen1Pokemon list is blank");
-        }
-    }
-}
-async function getPokemonList(upTo:number):Promise<string[]> {
-    let arr:string[] = new Array();
-    for(let i = 0; i<upTo; i++) {
-        getPkmnNameByDexNo(i).then(tempName => {
-            arr.push(tempName);
-            console.log(tempName);
-        });
-    }
-    return arr;
-}
+// // ======================================================================
+// export class getList {
+//     // static allPokemon():string[] {}
+//     static allGen1Pokemon():string[] {
+//         while(true) { // TODO: '-'
+//             getPokemonList(151).then(list => {
+//                 return list as string[];
+//             });
+//             console.error("allGen1Pokemon list is blank");
+//         }
+//     }
+// }
+// async function getPokemonList(upTo:number):Promise<string[]> {
+//     let arr:string[] = new Array();
+//     for(let i = 0; i<upTo; i++) {
+//         getPkmnNameByDexNo(i).then(tempName => {
+//             arr.push(tempName);
+//             console.log(tempName);
+//         });
+//     }
+//     return arr;
+// }
+
 // ======================================================================
 
-function capitalizeFirstLetter(name:string) {
-    return name.charAt(0).toUpperCase() + name.slice(1);
-}
+// export function capitalizeFirstLetter(name:string) {
+//     return name.charAt(0).toUpperCase() + name.slice(1);
+// }
 
 export default {
     pokeApi, 
     getPkmnNameByDexNo, 
     // getDexNoByName,
     // getPokemonSprite, 
-    getList, 
-    capitalizeFirstLetter
+    // getList, 
+    // capitalizeFirstLetter
 };
